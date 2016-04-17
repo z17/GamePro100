91c4995c0fb633e276e99fc52a91a46cf2972a06
Alex <zmn.wtr@gmail.com>
2016-04-17 12:02:20 +0300

    Add restart level

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -8,30 +8,90 @@ var myCodeMirror = CodeMirror(codeContainer, {
 myCodeMirror.setOption("theme", 'icecoder');
 myCodeMirror.setSize($(codeContainer).width(), $(codeContainer).height() );
 
-var worldMap = [];
-$.ajax({
-	url: '/services/task/getMap?id=1',
-	async: false
-}).done(function (data) {
-	worldMap = data;
-});
+var TILE_SIZE = 50;
 
-var TILE_SIZE = 50,
-	MAP_WIDTH = worldMap[0].length,
-	MAP_HEIGHT = worldMap.length;
-
-function renderObjectToMap(imagePath, coordX, coordY, container) {
-	var objectImage = PIXI.Texture.fromImage(imagePath);
+var humanImage = PIXI.Texture.fromImage('assets/jenya.png');
 
 // create a new Sprite using the texture
-	var object = new PIXI.Sprite(objectImage);
+var human = new PIXI.Sprite(humanImage);
+
+function initLevel() {
+	var worldMap = [];
+	$.ajax({
+		url: '/services/task/getMap?id=1',
+		async: false
+	}).done(function (data) {
+		worldMap = data;
+	});
+
+	var MAP_WIDTH = worldMap[0].length,
+		MAP_HEIGHT = worldMap.length;
+
+	function renderObjectToMap(imagePath, coordX, coordY, container) {
+		var objectImage = PIXI.Texture.fromImage(imagePath);
+
+// create a new Sprite using the texture
+		var object = new PIXI.Sprite(objectImage);
 
 // move the sprite to the center of the screen
-	object.position.x = TILE_SIZE * coordX;
-	object.position.y = TILE_SIZE * coordY;
+		object.position.x = TILE_SIZE * coordX;
+		object.position.y = TILE_SIZE * coordY;
 
-	container.addChild(object);
+		container.addChild(object);
+	}
+
+	var renderer = PIXI.autoDetectRenderer(TILE_SIZE * MAP_WIDTH, TILE_SIZE * MAP_HEIGHT, {backgroundColor : 0xECF0F1});
+	document.getElementById('game_container').appendChild(renderer.view);
+
+// create the root of the scene graph
+	var stage = new PIXI.Container();
+
+	var container = new PIXI.Container();
+
+	stage.addChild(container);
+
+	var startPosition = {
+		x: 0,
+		y: 0
+	};
+
+	for (var line in worldMap) {
+		for (var column in worldMap[line]) {
+			var terrainChar = worldMap[line][column],
+				imagePath = getPathByTerrainChar(terrainChar);
+			renderObjectToMap(imagePath, column, line, container);
+			if (terrainChar === 'S') {
+				startPosition.x = column;
+				startPosition.y = line;
+			}
+		}
+	}
+
+
+
+	human.anchor.y = 0.3;
+// move the sprite to the center of the screen
+	human.position.x = TILE_SIZE * startPosition.x;
+	human.position.y = TILE_SIZE * startPosition.y;
+
+	stage.addChild(human);
+
+
+
+	container.x = 0;
+	container.y = 0;
+	
+	// start animating
+	animate();
+
+	function animate() {
+		requestAnimationFrame(animate);
+
+		// render the root container
+		renderer.render(stage);
+	}
 }
+initLevel();
 
 function getPathByTerrainChar(terrainChar) {
 	var path = 'assets/';
@@ -56,60 +116,6 @@ function getPathByTerrainChar(terrainChar) {
 			break;
 	}
 	return path;
-}
-var renderer = PIXI.autoDetectRenderer(TILE_SIZE * MAP_WIDTH, TILE_SIZE * MAP_HEIGHT, {backgroundColor : 0xECF0F1});
-document.getElementById('game_container').appendChild(renderer.view);
-
-// create the root of the scene graph
-var stage = new PIXI.Container();
-
-var container = new PIXI.Container();
-
-stage.addChild(container);
-
-var startPosition = {
-	x: 0,
-	y: 0
-};
-
-for (var line in worldMap) {
-	for (var column in worldMap[line]) {
-		var terrainChar = worldMap[line][column],
-			imagePath = getPathByTerrainChar(terrainChar);
-		renderObjectToMap(imagePath, column, line, container);
-		if (terrainChar === 'S') {
-			startPosition.x = column;
-			startPosition.y = line;
-		}
-	}
-}
-
-
-var humanImage = PIXI.Texture.fromImage('assets/jenya.png');
-
-// create a new Sprite using the texture
-var human = new PIXI.Sprite(humanImage);
-
-human.anchor.y = 0.3;
-// move the sprite to the center of the screen
-human.position.x = TILE_SIZE * startPosition.x;
-human.position.y = TILE_SIZE * startPosition.y;
-
-stage.addChild(human);
-
-
-
-container.x = 0;
-container.y = 0;
-
-// start animating
-animate();
-
-function animate() {
-	requestAnimationFrame(animate);
-
-	// render the root container
-	renderer.render(stage);
 }
 
 function moveObject(object, direction) {
@@ -156,7 +162,6 @@ function moveObject(object, direction) {
 		}
 
 		var animationID = requestAnimationFrame(animateObject);
-		renderer.render(stage);
 
 		if (direction === 'RIGHT' && human.position.x >= targetPosition ||
 			direction === 'LEFT' && human.position.x <= targetPosition ||
@@ -199,7 +204,9 @@ function dieDieDieMyDarling(object) {
 	}
 }
 
-$('.js-start').click(function () {
+var popup = new $.Popup();
+
+$('body').on('click', '.js-start', function () {
 	$.ajax({
 		url: '/services/task/submit/',
 		data: { code: myCodeMirror.getValue(), id: 1 }
@@ -219,21 +226,25 @@ $('.js-start').click(function () {
 					if (command === 'ERROR') {
 						dieDieDieMyDarling(human);
 						$(this).unbind('animationEnd');
+						popup.open('<h3>Вы метрвы =(</h3><button class="button js-restart">Перезагрузить</button>', 'html');
 					} else if (command === 'FAIL') {
-						var popup = new $.Popup();
-						popup.open('<h3>Вы не выполнили предначертанное =(</h3>', 'html');
+						popup.open('<h3>Вы не выполнили предначертанное =(</h3><button class="button js-restart">Перезагрузить</button>', 'html');
 					} else if (command === 'FINISH') {
-						var popup = new $.Popup();
-						popup.open('<h3>Это успех!</h3>', 'html');
+						popup.open('<h3>Это успех!</h3><button class="button js-restart">Перезагрузить</button>', 'html');
 					} else if (command) {
 						moveObject(human, command);
 					}
 				});
 			}
 		} else {
-			var popup = new $.Popup();
 			popup.open('<h3>Ошибка исполнения кода:</h3><pre>' + dataObject.text + '</pre>', 'html');
 		}
 	});
 
+});
+
+$('body').on('click', '.js-restart', function (e) {
+	popup.close();
+	document.getElementById('game_container').innerHTML = "";
+	initLevel();
 });
