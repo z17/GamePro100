b59f00310d23b217ea94d9d726e10019c56becb9
Maksim Iliukovich <maksim_iliukovich@epam.com>
2016-05-09 02:48:32 +0300

    spring security custom login

--- src/main/java/com/hackday/controller/UserController.java
+++ src/main/java/com/hackday/controller/UserController.java
@@ -1,53 +1,48 @@
 package com.hackday.controller;
 
 import com.hackday.constants.Controllers;
+import com.hackday.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(Controllers.BASE_PATH + Controllers.USER)
 public class UserController {
 
-//    @Autowired
-//    AuthenticationManager authenticationManager;
-//
-//    @RequestMapping(value = Controllers.LOGIN, method = RequestMethod.GET)
-//    public LoginStatus login(@RequestParam(value = Controllers.PARAM_LOGIN) final String login,
-//                    @RequestParam(value = Controllers.PARAM_PASSWORD) final String password) throws IOException {
-//
-//        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(login, password);
-//        UserEntity details = new UserEntity();
-//        details.setLogin(login);
-//        token.setDetails(details);
-//
-//        try {
-//            Authentication auth = authenticationManager.authenticate(token);
-//            SecurityContextHolder.getContext().setAuthentication(auth);
-//            return new LoginStatus(auth.isAuthenticated(), auth.getName());
-//        } catch (BadCredentialsException e) {
-//            return new LoginStatus(false, null);
-//        }
-//    }
-//
-//    public class LoginStatus {
-//
-//        private final boolean loggedIn;
-//        private final String username;
-//
-//        public LoginStatus(boolean loggedIn, String username) {
-//            this.loggedIn = loggedIn;
-//            this.username = username;
-//        }
-//
-//        public boolean isLoggedIn() {
-//            return loggedIn;
-//        }
-//
-//        public String getUsername() {
-//            return username;
-//        }
-//    }
-//
+    @Autowired
+    @Qualifier("authenticationManager")
+    AuthenticationManager authenticationManager;
+
+    @RequestMapping(value = Controllers.LOGIN, method = RequestMethod.GET)
+    public boolean login(@RequestParam(value = Controllers.PARAM_LOGIN) final String login,
+                    @RequestParam(value = Controllers.PARAM_PASSWORD) final String password) throws IOException {
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(login, password);
+        UserEntity details = new UserEntity();
+        details.setLogin(login);
+        token.setDetails(details);
+
+        try {
+            Authentication auth = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            return auth.isAuthenticated();
+        } catch (BadCredentialsException | InternalAuthenticationServiceException e) {
+            return false;
+        }
+    }
+
 //    @RequestMapping(value = Controllers.LOGOUT, method = RequestMethod.GET)
 //    public Boolean logout() throws IOException {
 //        return true;
