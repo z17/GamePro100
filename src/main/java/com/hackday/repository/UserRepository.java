9c3391fd49ddad62df7b7a13f405047427f9a95a
Maksim Iliukovich <maksim_iliukovich@epam.com>
2016-10-16 17:26:10 +0300

    Remove old files

--- src/main/java/com/hackday/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hackday.repository;
-
-import com.hackday.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-    UserEntity findByLogin(String login);
-}
